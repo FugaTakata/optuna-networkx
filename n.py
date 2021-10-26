c3beb4e8ac9fd70f5825adb9aea2e116cd183b41
FugaTakata <maple1213goo@gmail.com>
2021-10-26 09:40:13 +0900

    add shape-based metrics

--- n.py
+++ n.py
@@ -28,6 +28,54 @@ def stress(pos, shortest_paths):
     return s
 
 
+def k_nearest(pos, k):
+    G = nx.DiGraph()
+
+    keys = list(pos.keys())
+    n = len(keys)
+
+    ds = {}
+    for si in range(0, n):
+        ds[keys[si]] = []
+        [sx, sy] = pos[keys[si]]
+        for ti in range(0, n):
+            [tx, ty] = pos[keys[ti]]
+            dx = abs(sx - tx)
+            dy = abs(sy - ty)
+            d = (dx ** 2 + dy ** 2) ** 0.5
+            ds[keys[si]].append({'id': keys[ti], 'd': d})
+
+    for si in range(0, n):
+        ds[keys[si]].sort(key=lambda x: x['d'])
+
+    G.add_nodes_from(keys)
+    for si in range(0, n):
+        for t in ds[keys[si]][:k]:
+            G.add_edge(keys[si], t['id'])
+
+    return G
+
+
+def jaccard_similarity(G, S):
+    s = 0
+    for node in G.nodes:
+        g_n = [n for n in G.neighbors(node)]
+        s_n = [n for n in S.neighbors(node)]
+        s += len(set(g_n) & set(s_n)) / len(set(g_n + s_n))
+
+    js = s / len(G.nodes)
+    return js
+
+
+def shape_based(G, pos):
+    k = 10
+    # generate shape graph, k may be a hyperparams
+    S = k_nearest(pos, k)
+    js = jaccard_similarity(G, S)
+
+    return js
+
+
 def objective_variable_graph(G, shortest_paths):
     def objective(trial):
         k = trial.suggest_float('k', 0, 1)
@@ -35,6 +83,8 @@ def objective_variable_graph(G, shortest_paths):
 
         pos = nx.spring_layout(G, iterations=i, threshold=1e-4, k=k)
         stress_value = stress(pos, shortest_paths)
+        shape_based_value = shape_based(G, pos)
+
         return stress_value
 
     return objective
@@ -53,9 +103,6 @@ def main():
                 source=ns,
                 target=nt)
 
-    study = optuna.create_study()
-    study.optimize(objective_variable_graph(G, shortest_paths), n_trials=100)
-
     best_params = study.best_params
     found_k = best_params['k']
     found_i = best_params['i']
@@ -73,173 +120,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-################################################################################################################
-################################################################################################################
-################################################################################################################
-################################################################################################################
-################################################################################################################
-################################################################################################################
-
-# seed = np.random.seed(10)
-
-# G = nx.Graph()
-
-# nodes = []
-# edges = []
-# for node in data.data['nodes']:
-#     nodes.append((node['id'], {'name': node['name']}))
-# for edge in data.data['links']:
-#     edges.append((edge['source'], edge['target']))
-
-# G.add_nodes_from(nodes)
-# G.add_edges_from(edges)
-
-
-# def stress(pos, g):
-#     keys = list(pos.keys())
-#     n = len(keys)
-#     s = 0
-
-#     for i in range(0, n):
-#         for j in range(i + 1, n):
-#             ki = keys[i]
-#             kj = keys[j]
-#             dx = pos[ki][0] - pos[kj][0]
-#             dy = pos[ki][1] - pos[kj][1]
-#             norm = (dx ** 2 + dy ** 2) ** 0.5
-#             if nx.has_path(g, source=kj, target=ki):
-#                 dij = nx.shortest_path_length(
-#                     g,
-#                     source=kj,
-#                     target=ki,
-#                 )
-#                 e = (norm - dij)
-#                 s += e ** 2
-#     return s
-
-
-# def objective(trial):
-#     k = trial.suggest_float('k', 0, 1)
-#     i = trial.suggest_int('i', 50, 200)
-#     pos = nx.spring_layout(G, iterations=i, threshold=1e-4, k=k)
-
-#     return stress(pos, G)
-
-
-# study = optuna.create_study()
-# study.optimize(objective, n_trials=200)
-
-# best_params = study.best_params
-# found_k = best_params['k']
-# found_i = best_params['i']
-
-# print(f'found k: {found_k}, found i: {found_i}')
-
-
-# found_pos = nx.spring_layout(G, iterations=found_i, threshold=1e-4, k=found_k)
-# nx.draw(G, pos=found_pos)
-
-
-# plt.show()
-
-
-# # import data
-
-# np.random.seed(10)
-
-# G = nx.Graph()
-
-# url = 'http://localhost:3000/api/n'
-
-# nodes = []
-# edges = []
-# for node in d.d['nodes']:
-#     # nodes.append((node['id'], {'name': node['name']}))
-#     nodes.append((node['id'], {'name': node['id']}))
-# for edge in d.d['links']:
-#     # edges.append((edge['source']['id'], edge['target']['id']))
-#     edges.append((edge['source'], edge['target']))
-
-# G.add_nodes_from(nodes)
-# G.add_edges_from(edges)
-
-# path = {}
-
-# for ns in nodes:
-#     path[ns[0]] = {}
-#     for nt in nodes:
-#         if nx.has_path(G, source=ns[0], target=nt[0]):
-#             p = nx.shortest_path_length(G,  source=ns[0], target=nt[0])
-#             path[ns[0]][nt[0]] = p
-
-# print(path)
-
-
-# def stress(pos, g):
-#     keys = list(pos.keys())
-#     n = len(keys)
-#     s = 0
-#     for i in range(0, n):
-#         for j in range(i + 1, n):
-#             ki = keys[i]
-#             kj = keys[j]
-#             dx = pos[ki][0] - pos[kj][0]
-#             dy = pos[ki][1] - pos[kj][1]
-#             norm = (dx ** 2 + dy ** 2) ** 0.5
-#             # if nx.has_path(g, source=kj, target=ki):
-#             if ki in path and kj in path[ki]:
-#                 # dij = nx.shortest_path_length(
-#                 #     g,
-#                 #     source=kj,
-#                 #     target=ki,
-#                 # )
-#                 dij = path[ki][kj]
-#                 e = (norm - dij)
-#                 s += e ** 2
-#     return s
-
-
-# def objective(trial):
-#     # k = trial.suggest_float('k', 0, 1)
-#     # i = trial.suggest_int('i', 50, 200)
-#     # pos = nx.spring_layout(G, iterations=i, threshold=1e-4, k=k)
-
-#     c = trial.suggest_float('c', -2, 2)
-#     x = trial.suggest_float('x', 0, 2)
-#     y = trial.suggest_float('y', 0, 2)
-
-#     print(url + f'?c={c}&x={x}&y={y}')
-#     res = requests.get(url + f'?c={c}&x={x}&y={y}')
-#     obj = json.loads(res.text)
-#     pos = {}
-#     for d in obj['nodes']:
-#         pos[d['id']] = [d['x'] / 1000,  d['y']/1000]
-#         # pos[d['id']] = [d['x'],  d['y']]
-#     return stress(pos, G)
-
-
-# study = optuna.create_study()
-# study.optimize(objective, n_trials=59)
-
-# best_params = study.best_params
-# fc = best_params['c']
-# fx = best_params['x']
-# fy = best_params['y']
-
-# print(f'found c: {fc}, found x: {fx}, found y: {fy}')
-
-
-# # found_pos = nx.spring_layout(G, iterations=found_i, threshold=1e-4, k=found_k)
-# res = requests.get(url + f'?c={fc}&x={fx}&y={fy}')
-# obj = json.loads(res.text)
-# # print(obj)
-# fpos = {}
-# for d in obj['nodes']:
-#     fpos[d['id']] = [d['x'] / 1000,  d['y']/1000]
-#     # fpos[d['id']] = [d['x'],  d['y']]
-# nx.draw(G, pos=fpos)
-
-
-# plt.show()
